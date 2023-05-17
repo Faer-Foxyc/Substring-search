a579654ac262414d0a68a0407335a36bc74bc84b
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-05-17 10:03:53 +0300

    Добавьте файлы проекта.

---
+++ Classes/Program.cs
@@ -0,0 +1,120 @@
+﻿using Substring_search.Classes;
+
+/*---------------Start program-----------------*/
+
+StartProgram();
+EndProgram();
+
+/*--------------------------------------------*/
+
+void StartProgram()
+{
+    Greeting();
+
+    while (true)
+    {
+        string EneteredString = ReturnAString();
+        string SubString = ReturnASubString();
+
+        Searcher searcher = new Searcher(EneteredString, SubString);
+        string answer = searcher.ReturnAString();
+
+        if (answer != "False")
+        {
+            Console.WriteLine($"\nВ строке: {EneteredString}, присутствует подстрока: {SubString}");
+        }
+        else
+        {
+            Console.WriteLine("\nПодстрока не найдена");
+        }
+
+        Console.Write("\nХотите продолжить? Да/Нет: ");
+        string _userInput = Console.ReadLine();
+        if (_userInput.ToLower() == "нет")
+        {
+            break;
+        }
+    }
+}
+// приветствие пользователя
+static void Greeting()
+{
+    string _userName = Environment.UserName;
+    Console.WriteLine("-------------------------------------------------------------------\n");
+    Console.WriteLine($"\tПриветствую тебя, пользователь {_userName}\n\tЭто простая программа для поиска подстроки в строке");
+    Console.WriteLine("\n-------------------------------------------------------------------");
+}
+
+/// конец програмы
+static void EndProgram()
+{
+    Searcher searcher = new Searcher();
+    Console.WriteLine(searcher.Rignts());
+    Thread.Sleep(5000);
+}
+
+// вернуть введенную подстроку
+static string ReturnASubString() 
+{
+    string _subString = string.Empty;
+
+    Console.Write("Введите подстроку для поиска: ");
+    _subString = Console.ReadLine();
+
+    while (true)
+    {
+        Searcher searcher = new Searcher();
+        string isString = searcher.CheckString(_subString);
+
+        // проверяем на null или пустую строку
+        if (string.IsNullOrEmpty(_subString))
+        {
+            Console.Write("\nСтрока не может быть пустой!\nВведите строку: ");
+            _subString = Console.ReadLine();
+        }
+        // проверяем на null, пробелы и неявляется ли строка числом
+        else if (string.IsNullOrWhiteSpace(_subString) || isString == false.ToString())
+        {
+            Console.WriteLine("\nВведенное значение не является строкой");
+            Console.Write("\nВведите строку для поиска: ");
+            _subString = Console.ReadLine();
+        }
+        else
+        {
+            break;
+        }
+    }
+    // возвращаем результат
+    return _subString;
+}
+/// вывести строку для 
+static string ReturnAString()
+{
+    string _enteredString = string.Empty;
+
+    Console.Write("\nВведите строку: ");
+    _enteredString = Console.ReadLine();
+
+    while (true)
+    {
+        Searcher searcher = new Searcher();
+        string isString = searcher.CheckString(_enteredString);
+
+        if (string.IsNullOrEmpty(_enteredString))
+        {
+            Console.Write("\nСтрока не может быть пустой!\nВведите строку: ");
+            _enteredString = Console.ReadLine();
+        }
+        else if (string.IsNullOrWhiteSpace(_enteredString) || isString == false.ToString())
+        {
+            Console.WriteLine("Введенное значение не является строкой");
+            Console.Write("\nВведите строку: ");
+            _enteredString = Console.ReadLine();
+        }
+        else
+        {
+            break;
+        }
+    }
+    return _enteredString;
+}
---
+++ Classes/Searcher.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Substring_search.Classes
+{
+    internal class Searcher
+    {
+        string enteredString { get; set; }
+        string subString { get; set; }
+        string allRignts { get; set; }
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="_eneteredString">строка в которой выполняется поиск</param>
+        /// <param name="_substring">строка которую нужно найти</param>
+        public Searcher(string _eneteredString, string _substring) 
+        {
+            enteredString = _eneteredString;
+            subString = _substring;
+        }
+        /// <summary>
+        /// Конструктор класса без параметров
+        /// </summary>
+        public Searcher() { }
+
+        /// <summary>
+        /// Проверить тип данных входной строки
+        /// </summary>
+        /// <param name="_userInput">входная строка</param>
+        /// <returns></returns>
+        public string CheckString(string _userInput)
+        {
+            int intValue;
+            double doubleValue;
+            bool isString = true;
+
+            if (int.TryParse(_userInput, out intValue))
+            {
+                isString = false;
+                return isString.ToString();
+            }
+            else if (double.TryParse(_userInput, out doubleValue))
+            {
+                isString = false;
+                return isString.ToString();
+            }
+            return isString.ToString();
+        }
+
+        /// <summary>
+        /// найти подстоку
+        /// </summary>
+        /// <returns></returns>
+        public string ReturnAString()
+        {
+            if (string.IsNullOrWhiteSpace(enteredString) || string.IsNullOrEmpty(subString))
+            {
+                return false.ToString();
+            }
+            else
+            {
+                int index = enteredString.IndexOf(subString);
+                if (index == -1)
+                {
+                    return false.ToString();
+                }
+                return true.ToString();
+            }
+        }
+        /// <summary>
+        /// вывод сообщения о защите прав)
+        /// </summary>
+        /// <returns></returns>
+        public string Rignts()
+        {
+            allRignts = "\nВсе права защищены, автором даной програмы является Foxy 17/05/2023";
+            return allRignts;
+        }
+    }
+}
